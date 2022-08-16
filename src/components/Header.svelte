0e4436df9da852638cc00a7aba235f99741504ae
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-15 21:51:31 -0300

    add github profile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,9 @@
 <script>
-	import logo from '../../static/apple-logo.png';
+	import github from '../../static/github-64.png';
 </script>
 
 <div class="bg-black w-full p-2 px-10 md:px-20 lg:px-52">
-	<img src={logo} alt="logo" class="h-6" />
+	<a href="https://github.com/hgoo150925/itunes-search">
+		<img src={github} alt="github-logo" target="_blank" class="h-6" />
+	</a>
 </div>
