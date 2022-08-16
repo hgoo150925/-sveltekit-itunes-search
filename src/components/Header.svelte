fee588c947ad2dcefd667a0c6f7c00ff57127fc8
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-15 21:52:41 -0300

    add github profile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,6 +4,6 @@
 
 <div class="bg-black w-full p-2 px-10 md:px-20 lg:px-52">
 	<a href="https://github.com/hgoo150925/itunes-search">
-		<img src={github} alt="github-logo" target="_blank" class="h-6" />
+		<img src={github} alt="github-logo" target="_blank" class="h-8" />
 	</a>
 </div>
